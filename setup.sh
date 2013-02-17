87eba68caf1f41066bf57f8e6e4834be91332cc3
Ronald Kaiser <raios.catodicos@gmail.com>
2013-02-17 13:52:50 -0300

    minor refactorings (colors and logic)

--- setup.sh
+++ setup.sh
@@ -15,29 +15,26 @@ function set_link {
     local TGT="$2"
 
     if [ -s "$SRC" ]; then
-        echo -e "\e[1;34m $SRC already exists. \e[0m"
+        echo -e "\e[0;34m $SRC already exists. \e[0m"
         return 1
     else
-        ln -s $TGT $SRC 
-        if [ "$?" = 0 ]; then
-            echo -e "\e[1;32m $SRC configured! \e[0m"
-        fi
+        ln -s $TGT $SRC && echo -e "\e[0;32m $SRC configured! \e[0m"
     fi
 }
 
 function set_ssh {
     echo -e "\e[1;33m[ssh]\e[0m"
-    set_link "$HOME/.ssh/config" "$PREFIX/ssh/config"
+    set_link "$HOME/.ssh/config" "$PREFIX/ssh/config" && task_done
 }
 
 function set_vim {
     echo -e "\e[1;33m[vim]\e[0m"
-    set_link "$HOME/.vimrc" "$PREFIX/vim/vimrc" 
+    set_link "$HOME/.vimrc" "$PREFIX/vim/vimrc" && task_done
 }
 
 function set_git {
     echo -e "\e[1;33m[git]\e[0m"
-    set_link "$HOME/.gitconfig" "$PREFIX/git/gitconfig"
+    set_link "$HOME/.gitconfig" "$PREFIX/git/gitconfig" && task_done
 }
 
 function add_line {
@@ -45,17 +42,17 @@ function add_line {
     local FILE="$2"
         
     if [ ! -f "$FILE" ]; then
-        echo -e "\e[1;34mFile: $FILE does not exists. \e[0m"
+        echo -e "\e[0;34mFile: $FILE does not exist. \e[0m"
         return 1        
     fi
     grep "$LINE" "$FILE"
     if [ ! "$?" = 0 ]; then
         echo "$LINE" >> "$FILE"
         if [ "$?" = 0 ]; then
-            echo -e "\e[1;32mLine: \"$LINE\" added to $FILE!\e[0m"
+            echo -e "\e[0;32mLine: \"$LINE\" added to $FILE!\e[0m"
         fi
     else
-        echo -e "\e[1;34mLine: \"$LINE\" already exists in $FILE. \e[0m"
+        echo -e "\e[0;34mLine: \"$LINE\" already exists in $FILE. \e[0m"
     fi
 }
     
