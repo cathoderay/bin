62ef40fd84bdfc926e144631df02774e94658ae6
Ronald Kaiser <raios.catodicos@gmail.com>
2013-02-17 13:33:00 -0300

    adding colors and short-circuits

--- setup.sh
+++ setup.sh
@@ -6,54 +6,80 @@
 
 PREFIX="$HOME/bin/dotfiles"
 
+function task_done {
+    echo -e "\e[1;32mDone!\e[0m"
+}
+
 function set_link {
-    SRC="$1"
-    TGT="$2"
+    local SRC="$1"
+    local TGT="$2"
 
     if [ -s "$SRC" ]; then
-        echo "$SRC already exists."
+        echo -e "\e[1;34m $SRC already exists. \e[0m"
+        return 1
     else
         ln -s $TGT $SRC 
         if [ "$?" = 0 ]; then
-            echo "$SRC configured!"
+            echo -e "\e[1;32m $SRC configured! \e[0m"
         fi
     fi
 }
 
 function set_ssh {
+    echo -e "\e[1;33m[ssh]\e[0m"
     set_link "$HOME/.ssh/config" "$PREFIX/ssh/config"
 }
 
 function set_vim {
+    echo -e "\e[1;33m[vim]\e[0m"
     set_link "$HOME/.vimrc" "$PREFIX/vim/vimrc" 
 }
 
 function set_git {
+    echo -e "\e[1;33m[git]\e[0m"
     set_link "$HOME/.gitconfig" "$PREFIX/git/gitconfig"
 }
 
 function add_line {
-    LINE="$1"
-    FILE="$2"
+    local LINE="$1"
+    local FILE="$2"
         
+    if [ ! -s "$FILE" ]; then
+        echo -e "\e[1;34mFile: $FILE does not exists. \e[0m"
+        return 1        
+    fi
     grep "$LINE" "$FILE"
     if [ ! "$?" = 0 ]; then
         echo "$LINE" >> "$FILE"
+        if [ "$?" = 0 ]; then
+            echo -e "\e[1;32mLine: \"$LINE\" added to $FILE!\e[0m"
+        fi
+    else
+        echo -e "\e[1;34mLine: \"$LINE\" already exists in $FILE. \e[0m"
     fi
 }
     
 function set_bashmarks {
+    echo -e "\e[1;33m[bashmarks]\e[0m"
     git clone git://github.com/huyng/bashmarks.git /tmp/bashmarks
-    if [ "$?" = 0 ]; then
-        cd /tmp/bashmarks && make install >/dev/null
-        add_line ". ~/.local/bin/bashmarks.sh" "$PREFIX/bashrc"
-        cd -
-        rm -rf /tmp/bashmarks
+    if [ ! "$?" = 0 ]; then
+        echo -e "\e[1;31mProblem during repository cloning.\e[0m"
+        return 1
     fi
+
+    cd /tmp/bashmarks && 
+        make install >/dev/null &&
+        add_line ". ~/.local/bin/bashmarks.sh" "$PREFIX/bashrc" &&
+        cd - &&
+        rm -rf /tmp/bashmarks && 
+        task_done
 }
 
 function set_bashrc {
-    touch ~/.bashrc && add_line ". $PREFIX/bashrc" ~/.bashrc
+    echo -e "\e[1;33m[bashrc]\e[0m"
+    touch ~/.bashrc && 
+        add_line ". $PREFIX/bashrc" ~/.bashrc &&
+        task_done
 }
 
 case $1 in
